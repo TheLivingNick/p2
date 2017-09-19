db594a84edacb1e4dd5b682283360b86e86a3ee6
Nick Troy <nick.troy@gmail.com>
2017-09-19 18:39:12 -0400

    Adding a test index.php to make sure subdomain p2 is working

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>DWA15 Project 2</title>
+        <!-- <link rel="stylesheet" href="css/dwa15-p1.css"> -->
+    </head>
+    
+    <body>
+        <p>test index</p>
+    </body>
+    
+</html>
