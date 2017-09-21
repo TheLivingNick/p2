dddb5991bfe04add659aa99e0f11342d21a35c74
Nick Troy <nick.troy@gmail.com>
2017-09-20 21:25:21 -0400

    Push of finished page, hopefully works on production server

--- index.php
+++ index.php
@@ -4,22 +4,40 @@
 <head>
   <meta charset="utf-8"/>
   <title>DWA15 Project 2</title>
-  <!-- <link rel="stylesheet" href="css/dwa15-p1.css"> -->
+  <link rel="stylesheet" href="css/scrabbleScore.css">
 </head>
 
 <body>
 
-  <p>test index</p>
+  <img src="images\scrabble-letters.jpg" alt="Scrabble Letters" height="277px" width="333px">
+
+  <h1>Scrabble Score Calculator</h1>
+  <h3>Want to find the value of a word? Enter it below, set the modifiers, and hit the button to find out!</h3>
 
   <form method="get">
 
-    <input type='text' name='userWord' id='userWord' value='<?=sanitize($userWord)?>'>
+    <label for='userWord'>Enter word here: </label>
+    <input type='text' class='monospace' name='userWord' id='userWord' size='18' maxlength='15' value='<?=sanitize($userWord)?>'>
 
-    <input type='radio' name='bonusMult' value='noBonus' <?php if ($bonusMult == "noBonus") echo 'CHECKED ' ?>>
-    <input type='radio' name='bonusMult' value='doubleScore' <?php if ($bonusMult == "doubleScore") echo 'CHECKED ' ?>>
-    <input type='radio' name='bonusMult' value='tripleeScore' <?php if ($bonusMult == "tripleScore") echo 'CHECKED ' ?>>
+    <p>Are there any modifiers?</p>
+    <label for='userWord'>No modifiers: </label>
+    <input type='radio' name='bonusMult' value='noBonus' <?php if ($bonusMult == "noBonus") echo 'CHECKED'?>>
+    <label for='userWord'>Double Wore Score: </label>
+    <input type='radio' name='bonusMult' value='doubleScore' <?php if ($bonusMult == "doubleScore") echo 'CHECKED'?>>
+    <label for='userWord'>Triple Word Score (nice!): </label>
+    <input type='radio' name='bonusMult' value='tripleScore' <?php if ($bonusMult == "tripleScore") echo 'CHECKED'?>>
 
-    <input type='checkbox' name='allLetters' id='allLetters' <?=$allLettersBonus?>>
+    <p>Should the "Used All Letters" bonus apply?</p>
+    <label for='sevenBonus'>Apply bonus: </label>
+    <input type='checkbox' name='sevenBonus' id='sevenBonus' <?=$sevenBonus?>>
+
+    <input type='submit' id='buttonSubmit' value='Find Score'>
+
+  </form>
+
+  <div class="<?=$resultType?>">
+    <?=$resultText?>
+  </div>
 
 </body>
 
--- scrabbleLogic.php
+++ scrabbleLogic.php
@@ -4,12 +4,70 @@ require('helpers.php');
 # Create asscociative array of letter values from JSON
 $letterValuesJSON = file_get_contents('json/letterValues.json');
 $letterValues = json_decode($letterValuesJSON, true);
-$bonusMult = 'noBonus';
-$allLettersBonus = '';
 
-# Retrieve data from the form
+# if a word was entered, keep it after submit, otherwise default to empty string
 if (isset($_GET['userWord'])) {
     $userWord = $_GET['userWord'];
 } else {
     $userWord = '';
+}
+
+# if the multiplier was changed from noBonus use the value in $_GET, other default to noBonus
+if (isset($_GET['bonusMult']) && $_GET['bonusMult'] != 'noBonus') {
+    $bonusMult = $_GET['bonusMult'];
+} else {
+    $bonusMult = 'noBonus';
+}
+
+# if the 7-letter bonus box was checked keep the check, otherwise default to blank
+if (isset($_GET['sevenBonus'])) {
+    $sevenBonus = 'CHECKED';
+} else {
+    $sevenBonus = '';
+}
+
+# check to see if there is an entered word, if the string is all letters, then computes the value of the word
+# if there is no entry a default result area is displayed; if there are non-letter characters, a bad result is displayed
+if ($userWord != '' && ctype_alpha($userWord)) {
+  $wordValue = 0;
+  $wordArray = str_split($userWord);
+
+  # loop through each letter and add its value to the total
+  foreach($wordArray as $index => $letter) {
+    $wordValue += $letterValues[strtolower($letter)];
+  }
+
+  # apply multipliers if there are any
+  if ($bonusMult == 'doubleScore') {
+    $wordValue += $wordValue;
+  } elseif ($bonusMult == 'tripleScore') {
+    $wordValue = $wordValue * 3;
+  } else {
+    $wordValue = $wordValue;
+  }
+
+  # if the Used All Letters bonus is checked and the word is long enough add the bonus
+  if ($sevenBonus == 'CHECKED' && strlen($userWord) >= 7) {
+    $wordValue += 50;
+  }
+
+  # set result area to haveResult and seed the text for it
+  $resultType = 'haveResult';
+  $resultText = '';
+
+  # add image of each letter in sequence to show word in tile form
+  foreach ($wordArray as $index => $letter) {
+    $resultText = $resultText."<img class=\"tilePic\" src=\"images\\letter-".$letter.".png\">";
+  }
+
+  # append the total value of the word
+  $resultText = $resultText." is worth ".$wordValue." points!";
+} elseif ($userWord != '' && !ctype_alpha($userWord)) {
+  #if result contains non-letters, show a bad result
+  $resultType = 'badResult';
+  $resultText = "Your entry can only contain letters.";
+} else {
+  # if there is no entered word, show a placeholder result
+  $resultType = 'noResult';
+  $resultText = "This is where your result will display!";
 }
